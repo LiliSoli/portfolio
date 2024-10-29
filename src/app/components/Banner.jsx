e06082969ca0666cd9b3ea17c2ec231b1afe9811
Aurélie Solier <solier.lili@gmail.com>
2024-10-29 17:59:25 +0100

    Suppression SkillCard

--- src/app/components/Banner.jsx
+++ src/app/components/Banner.jsx
@@ -41,43 +41,6 @@ const Banner = () => {
         </p>
       </div>
 
-
-        {/* <Image
-            width={1/3}
-            height={1/3}
-            layout="responsive"
-            src={`${process.env.NEXT_PUBLIC_BASE_PATH}assets/images/laptop-banner.jpg`}
-            alt="Avatar"
-            className="opacity-40"
-          /> */}
-
-
-
-      {/* <div className="relative w-1/2 h-fit p-8 mx-auto bg-gray-100 rounded-lg shadow-md text-gray-700 flex flex-col items-center justify-center ">
-          <Image
-            width={1/3}
-            height={1/3}
-            layout="responsive"
-            src={`${process.env.NEXT_PUBLIC_BASE_PATH}assets/images/laptop-banner.jpg`}
-            alt="Avatar"
-            className=""
-          />
-        <div className="absolute -left-24 top mb-8 rounded-full w-48 h-48 bg-gradient-to-r from-orange-300 to-red-300 flex items-center justify-center shadow-lg">
-          <Image
-            width={40}
-            height={40}
-            layout="responsive"
-            src={`${process.env.NEXT_PUBLIC_BASE_PATH}assets/images/avatargray.png`}
-            alt="Avatar"
-            className="rounded-full w-40 h-40"
-          />
-        </div>
-
-        <h1 className="text-3xl font-bold pt-24 mb-4 tracking-wider">BIENVENUE SUR MON <span className="text-green-400">PORTFOLIO !</span></h1>
-        <p className="text-xl mb-16 max-w-lg leading-relaxed">
-          Développeuse passionnée, je crée des applications web modernes et performantes. Découvrez mes projets ci-dessous ! Et voilà un test.
-        </p>
-      </div> */}
       <FontAwesomeIcon
         icon={faCircleChevronDown}
         className="text-white text-5xl cursor-pointer absolute bottom-10 transition-all duration-300 transform hover:scale-110 bg-clip-text"
